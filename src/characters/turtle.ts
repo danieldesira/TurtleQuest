8cdac7b4d2e29c54a87e94b873b2313224db1fb7
Daniel Desira <desiradaniel2007@gmail.com>
2024-01-03 22:47:55 +0100

    #1: Experimental turtle angulation

--- src/characters/turtle.ts
+++ src/characters/turtle.ts
@@ -1,43 +1,56 @@
+import angles from "../constants/angles";
 import Character from "./character";
 
 class Turtle implements Character {
   private x: number;
   private y: number;
+  private angle: number;
 
   constructor() {
     this.x = 10;
     this.y = 10;
+    this.angle = angles.right;
   }
 
   moveUp() {
+    this.rotate(angles.up);
     if (this.y >= 0) {
       --this.y;
     }
   }
 
   moveDown() {
+    this.rotate(angles.down);
     ++this.y;
   }
 
   moveLeft() {
+    this.rotate(angles.left);
     if (this.x >= 0) {
       --this.x;
     }
   }
 
   moveRight() {
+    this.rotate(angles.right);
     ++this.x;
   }
 
-  setPosition(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+  private rotate(angle: number) {
+    this.angle = angle;
+  }
+
+  private applyRotation(context: CanvasRenderingContext2D) {
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.translate(-this.x, -this.y);
   }
 
   paint(context: CanvasRenderingContext2D) {
     const turtleImage = document.createElement("img");
     turtleImage.src = "./images/turtle.png";
-    turtleImage.onload = () =>
+    turtleImage.onload = () => {
+      this.applyRotation(context);
       context.drawImage(
         turtleImage,
         this.x,
@@ -45,6 +58,8 @@ class Turtle implements Character {
         turtleImage.width / 4,
         turtleImage.height / 4
       );
+      context.resetTransform();
+    };
   }
 }
 
---
+++ src/constants/angles.ts
@@ -0,0 +1,8 @@
+const angles = {
+  up: 0,
+  right: Math.PI / 2,
+  down: Math.PI,
+  left: Math.PI + Math.PI / 2,
+};
+
+export default angles;
