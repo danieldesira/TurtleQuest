837bd4ce04040e7071d86785e041a7504be15621
Daniel Desira <desiradaniel2007@gmail.com>
2024-03-26 19:21:16 +0100

    #17: Complete Loading message with layout fixes

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import RootState from "../features/RootState";
 
 function LoadingIndicator() {
+  const levelNo = useSelector((state: RootState) => state.levels.level.value);
+
   return (
-    <div className="fixed top-0 left-0 flex justify-center items-center">
-      <span className="text-5xl">Loading Level ....</span>
+    <div className="fixed top-0 left-0 flex h-screen w-full items-center justify-center">
+      <span className="text-5xl text-white">Loading Level {levelNo} ...</span>
     </div>
   );
 }
