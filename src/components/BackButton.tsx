820d0e2f2a742c478acff64d30a61c943034cbc9
Daniel Desira <desiradaniel2007@gmail.com>
2024-04-28 23:44:16 +0200

    #30: Add visual back button

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { stopGame } from "../features/gameState/gameStateReducer";
+import Game from "../Game";
+import store from "../store";
+
+function BackButton() {
+  const dispatch = useDispatch();
+
+  const saveGame = () => {
+    const data = {
+      turtle: Game.instance.turtle,
+      characters: Game.instance.level.characters,
+      levelNo: store.getState().levels.level.value,
+    };
+    localStorage.setItem("currentGame", JSON.stringify(data));
+  };
+
+  const handleClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    saveGame();
+    dispatch(stopGame());
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      className="bg-black opacity-60 hover:opacity-90 rounded-xl p-2"
+    >
+      Menu
+    </button>
+  );
+}
+
+export default BackButton;
--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import LevelText from "./LevelText";
 import MeterGroup from "./meters/MeterGroup";
 import XPText from "./XPText";
+import BackButton from "./BackButton";
 
 function GameHeader() {
   return (
     <div className="fixed top-1 left-1 flex dark:text-white gap-5 items-center">
+      <BackButton />
       <MeterGroup />
       <XPText />
       <LevelText />
