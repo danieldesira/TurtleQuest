78b4edc9bfbe5b5dac94c93b3ee5a67b87ffcdb0
Daniel Desira <desiradaniel2007@gmail.com>
2024-09-23 18:47:58 +0200

    #33: Fix behavior for Google signon

--- src/components/LoginButtons.tsx
+++ src/components/LoginButtons.tsx
@@ -1,13 +1,39 @@
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
-import React from "react";
+import {
+  CredentialResponse,
+  GoogleLogin,
+  GoogleOAuthProvider,
+} from "@react-oauth/google";
+import React, { useState } from "react";
 
 function LoginButtons() {
-  const handleSuccess = () => alert("hello");
+  const isAuthenticated = !!localStorage.getItem("authentication");
+
+  const [showLogin, setShowLogin] = useState<boolean>(!isAuthenticated);
+
+  const handleSuccess = ({ credential }: CredentialResponse) => {
+    localStorage.setItem("authentication", credential);
+    setShowLogin((_) => false);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("authentication");
+    setShowLogin((_) => true);
+  };
 
   return (
-    <div className="flex flex-col gap-2">
-      <GoogleOAuthProvider clientId="133557707337-19ctrvhko436b3aqf8f3mk3ms9l33nla.apps.googleusercontent.com">
-        <GoogleLogin onSuccess={handleSuccess} useOneTap />
+    <div className="flex flex-col gap-2 justify-center items-center">
+      <GoogleOAuthProvider clientId="133557707337-n53m2nodegdumjcsjccodb6fppaf6mcv.apps.googleusercontent.com">
+        {showLogin ? (
+          <GoogleLogin onSuccess={handleSuccess} useOneTap />
+        ) : (
+          <button
+            type="button"
+            className="bg-gray-800 text-white rounded-md p-2"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        )}
       </GoogleOAuthProvider>
     </div>
   );
