79294dc23af30029e66bb8225c9b30f23d8acf96
Daniel Desira <desiradaniel2007@gmail.com>
2025-08-02 17:05:14 +0200

    #51: Implement score dialog

--- src/components/InfoDisplay.tsx
+++ src/components/InfoDisplay.tsx
@@ -8,7 +8,7 @@ type Props = {
 const InfoDisplay = ({ title, content }: Props) => (
   <div className="bg-pink-600 opacity-80 hover:opacity-100 transition-opacity text-white p-2 rounded-sm gap-20">
     <h2 className="text-lg">{title}</h2>
-    <div className="overflow-auto max-h-[25vh]">{content}</div>
+    <div className="overflow-auto">{content}</div>
   </div>
 );
 
--- src/components/scores/LeaderBoard.tsx
+++ src/components/scores/LeaderBoard.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { fetchScores } from "../../services/api";
 import { GetScoresResponse } from "../../services/types";
+import Dialog from "../dialog/Dialog";
 import ScoreTable from "./ScoreTable";
 
 const LeaderBoard = () => {
   const [scores, setScores] = useState<GetScoresResponse>();
+  const [showScoresDialog, setShowScoresDialog] = useState<boolean>(false);
+
+  const handleViewScores = () => setShowScoresDialog(true);
+
+  const closeHighScores = () => setShowScoresDialog(false);
 
   useEffect(() => {
     fetchScores().then((res) => setScores(res));
@@ -14,7 +20,6 @@ const LeaderBoard = () => {
     <div className="flex flex-col gap-1">
       {scores ? (
         <>
-          <ScoreTable scores={scores} />
           <div className="text-sm">
             {scores.personalBest ? (
               <>
@@ -25,6 +30,14 @@ const LeaderBoard = () => {
               <>Go make a name for yourself. Start a new game!</>
             )}
           </div>
+          <button type="button" onClick={handleViewScores}>
+            View High Scores
+          </button>
+          {showScoresDialog && (
+            <Dialog title="High Scores" handleOk={closeHighScores}>
+              <ScoreTable scores={scores} />
+            </Dialog>
+          )}
         </>
       ) : (
         <>Loading scores...</>
